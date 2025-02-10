4c9b6a9e2e333eeea6daec20db59133da20b915a
toygr <milkyway464203@gmail.com>
2025-02-11 01:52:42 +0900

    bugfix: param sync with contract

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,14 +14,14 @@ export const LABELS = {
 export const timestamp2date = (timestamp_ms: number) => new Date(timestamp_ms).toLocaleDateString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })
 export const getVestingDuration = (totalAllocation: anchor.BN, vesting_duration_x1: anchor.BN) => {
     const val = parseInt(totalAllocation.div(new anchor.BN(1_000_000)))
-    if (val < 80_000) {
-        return 0
+    if (val < 200_000) {
+        return vesting_duration_x1
     } else if (val < 2_000_000) {
-        return parseInt(vesting_duration_x1)
-    } else if (val < 10_000_000) {
         return parseInt(vesting_duration_x1) * 2
+    } else if (val < 10_000_000) {
+        return parseInt(vesting_duration_x1) * 4
     } else {
-        return parseInt(vesting_duration_x1) * 3
+        return parseInt(vesting_duration_x1) * 6
     }
 }
 export const getCliffDuration = (totalAllocation: anchor.BN, vesting_duration_x1: anchor.BN) => {
@@ -29,15 +29,15 @@ export const getCliffDuration = (totalAllocation: anchor.BN, vesting_duration_x1
     if (val < 2_000_000) {
         return 0
     } else if (val < 10_000_000) {
-        return Math.floor(parseInt(vesting_duration_x1) / 2)
-    } else {
         return parseInt(vesting_duration_x1)
+    } else {
+        return parseInt(vesting_duration_x1) * 2
     }
 }
 export const getInitialUnlockRate = (totalAllocation: anchor.BN) => {
     const val = parseInt(totalAllocation.div(new anchor.BN(1_000_000)))
     if (val < 80_000) {
-        return 1
+        return 0.25
     } else if (val < 2_000_000) {
         return 0.2
     } else if (val < 10_000_000) {
